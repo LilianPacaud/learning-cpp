fbd0564d18658cfedce713747fb8e288f8bc1367
lilian <lilian.pacaud1@gmail.com>
2021-11-14 02:41:02 +0100

    add prime function

--- prime.cpp
+++ prime.cpp
@@ -2,29 +2,33 @@
 #include <list>
 
 using namespace std;
-
-int main()
+list<int> getPrimeNumbers()
 {
     int number = 2;
-    list<int> arrayPrimary{2};
+    list<int> arrayPrime{2};
     while (number < 200)
     {
-        bool is_primay = true;
+        bool is_prime = true;
         for (int i = 2; i < number; i++)
         {
             if (number % i == 0)
             {
-                is_primay = false;
+                is_prime = false;
             }
-            if (i == number - 1 && is_primay == true)
+            if (i == number - 1 && is_prime == true)
             {
-                arrayPrimary.push_back(number);
+                arrayPrime.push_back(number);
             }
         }
         number++;
     }
 
-    for (auto const &i : arrayPrimary)
+    return arrayPrime;
+}
+int main()
+{
+    list<int> arrayPrime = getPrimeNumbers();
+    for (auto const &i : arrayPrime)
     {
         std::cout << i << std::endl;
     }
